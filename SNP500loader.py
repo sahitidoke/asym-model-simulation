5f80566430783581da05ed53ce9e049a4b00cf36
olinostrauss <olinostrauss@gmail.com>
2026-08-03 18:04:17 -0700

    Finished snp500loader

---
+++ SNP500loader.py
@@ -0,0 +1,16 @@
+import pandas
+import numpy
+import csv
+
+def load_snp_500():
+    f = open('prices.csv','r')
+    d = csv.reader(f)
+
+    data_matrix = []
+
+    for row in d:
+        data_matrix += [row]
+    
+    return data_matrix
+
+
